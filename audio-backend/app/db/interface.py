9c5313217925a7f939544f28d8366dea0be7486d
ner216 <provencher.nolan@gmail.com>
2025-06-05 16:31:53 -0400

    fix: Fix errors with basic test functions in test scripts. Add function to interface that pauses execution until database is online.

--- audio-backend/app/db/interface.py
+++ audio-backend/app/db/interface.py
@@ -2,6 +2,8 @@ import psycopg2
 from psycopg2 import OperationalError
 from psycopg2.pool import SimpleConnectionPool
 import os
+import time
+import socket
 
 # Get envirnment variables from linux container
 DB_NAME = os.getenv("DB_NAME")
@@ -10,24 +12,77 @@ DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASSWORD")
 DB_PORT = os.getenv("DB_PORT")
 
+# A function to pause execution untill the database is online
+def wait_for_db(timeout=int):
+    time.sleep(4)
+    start_time = time.time()
+
+    while True:
+        try:
+            with socket.create_connection((DB_HOST, DB_PORT), timeout=2):
+                print(f"[INFO] Database is available at port {DB_PORT}")
+                return
+        except OSError:
+            print(f"[INFO] Waiting for database at {DB_HOST}:{DB_PORT}...")
+            time.sleep(1)
+            if time.time() - start_time > timeout:
+                print("[INFO] Server timeout reached waiting for db!")
+                exit()
+
+
+class conn_pool(object):
+    def __init__(self, min_conn, max_conn): # Take max and min db connections as parameter
+        self.min_conn = min_conn
+        self.max_conn = max_conn
+        self.db_pool = None
+
+        try:
+            self.db_pool = SimpleConnectionPool(
+                minconn=self.min_conn,
+                maxconn=self.max_conn,
+                database=DB_NAME,
+                user=DB_USER,
+                host=DB_HOST,
+                port=DB_PORT,
+                password=DB_PASS
+            )
+            print("[INFO] Successfully connected to database!")
+            print(f"[INFO] psycopg2 connection pool initialized with max={self.max_conn} and min={self.min_conn}")
+        except Exception as e:
+            print(f"[ERROR] Unable to initialize connection pool! [conn_pool::__init__]\n Error: {e}")
+            exit()
+
+    def get_conn(self): # Borrow a connection from the pool
+        try:
+            return self.db_pool.getconn()
+        except Exception as e:
+            print(f"[ERROR] Unable to borrow connection [conn_pool::get_conn]\n Error: {e}")
+            exit()
+    
+    def return_conn(self, conn):
+        try:
+            self.db_pool.putconn(conn)
+        except Exception as e:
+            print(f"[ERROR] Unable to return conn to pool! [conn_pool::return_conn]\n Error: {e}")
+            exit()
+    
+    def close_pool(self):
+        try:
+            self.db_pool.closeall()
+            print("[INFO] Pool has been closed!")
+        except Exception as e:
+            print(f"[ERROR] Unable to close conn pool! [conn_pool::close_pool]\n Error: {e}")
+
+
+
 class db_interface(object):
     def __init__(self):
-        # Connection and cursur variables for psycopg2
-        self.conn = None
-        self.curr = None
+        # Composition of conn_pool object for conn management
+        self.pool = conn_pool(1, 15)
 
         # Show current directory in container
         print(f"[INFO] Current directory '{os.getcwd()}' [db_interface::__init__]")
 
-
-    # A function to initialize the connection between the back-end and the database
-    def start(self):
-        try:
-            self.conn = psycopg2.connect(database=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST, port=DB_PORT)
-            print("[INFO] Connection to database successful! [db_interface::start]")
-        except Exception as e:
-            print(f"[ERROR] unable to connect to database. [db_interface::start] \nError: {e}")
-            return
 
     def load_schema(self, schema_file_path) -> bool:
         if not os.path.exists(schema_file_path):
@@ -38,56 +93,54 @@ class db_interface(object):
             with open(schema_file_path, "r") as file:
                 sql_script = file.read()
 
-            self.curr = self.conn.cursor()
-            self.curr.execute(sql_script) 
+            # Get connection from pool
+            conn = self.pool.get_conn()
+            curr = conn.cursor()
+            
+            curr.execute(sql_script)
             print(f"[INFO] Schema from '{schema_file_path}' loaded successfully! [db_interface::load_schema]")
             return True
         except psycopg2.Error as e:
             print(f"[ERROR] Schema file from '{schema_file_path}' could not be loaded/read! [db_interface::load_schema] \nError: {e}")
-            if not self.conn.autocommit:
-                self.conn.rollback()
+            if not conn.autocommit:
+                conn.rollback()
             return False
-        finally:
-            if self.curr:
-                self.curr.close()
-            if self.conn:
-                self.conn.close()
+        finally: # Return the conn after use
+            if curr:
+                conn.commit()
+                curr.close()
+            if conn:
+                self.pool.return_conn(conn)
 
 
     def execute_query(self, sql, params=None ,fetch_one=False, fetch_all=False, commit=False):
         try:
-            if self.conn == None:
-                return None # Return none if database is not connected
+            result = None
+            conn = self.pool.get_conn()
+            curr = conn.cursor()
             
-            self.curr.execute(sql, params)
+            curr.execute(sql, params)
             
             if fetch_all == True:
-                result = self.curr.fetchall()
+                result = curr.fetchall()
             elif fetch_one == True:
-                result = self.curr.fetchone()
+                result = curr.fetchone()
             elif commit == True: # If a commit is requested for a DML opteration
-                self.conn.commit()
+                conn.commit()
             else: # For DDL operation or SELECT where fetch is not needed. 
                 pass
+
         except psycopg2.Error as e:
             print(f"[ERROR] Unable fullfill transaction! [db_interface::execute_query]\n Error: {e}")
-            self.conn.rollback()
+            conn.rollback()
         except Exception as e:
             print(f"[ERROR] Unable fullfill transaction! [db_interface::execute_query]\n Error: {e}")
-            self.conn.rollback()
+            conn.rollback()
         finally:
-            if self.curr:
-                self.curr.close()
-            if self.conn:
-                self.conn.close()
+            if curr:
+                curr.close()
+            if conn:
+                conn.commit()
+                self.pool.return_conn(conn)
 
         return result
-
-
-    def test(self):
-        curr = self.conn.cursor()
-
-        curr.execute("SELECT * FROM Songs")
-
-        records = curr.fetchall()
-        print(records)
--- audio-backend/app/tests/db_interface_tests.py
+++ audio-backend/app/tests/db_interface_tests.py
@@ -13,9 +13,16 @@ def basic_query() -> bool:
     try:
         print("[INFO] Starting db basic query test...")
         interface = db_interface()
-        interface.start()
         interface.load_schema("app/db/bitbox_schema.sql")
-        interface.execute_query("SELECT * FROM Songs")
+
+        result = interface.execute_query(
+            "INSERT INTO users (username, email) VALUES ('example_user', 'email');",
+            commit=True
+            )
+        print(f"TEST INSERT:\n{result}")
+
+        result = interface.execute_query("SELECT * FROM Users;", fetch_all=True)
+        print(f"TEST SELECT:\n{result}")
         print("[INFO] TEST IS SUCCESSFULL!")
         return True
     except Exception as e:
