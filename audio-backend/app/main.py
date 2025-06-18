b7faf995abfefc445748e1d53b05658dd4292c93
ner216 <provencher.nolan@gmail.com>
2025-06-18 15:55:43 -0400

    fix: fix db_setup_stat env variable.

--- audio-backend/app/main.py
+++ audio-backend/app/main.py
@@ -5,18 +5,18 @@ from db.interface import wait_for_db, load_schema, scan_music_files
 import os
 
 # Stores if db is set up. 1 or 0
-#DB_SETUP_STAT = os.getenv("DB_SETUP_STAT")
+DB_SETUP_STAT = os.environ.get("DB_SETUP_STAT")
 
 # Wait for db to start
 wait_for_db(40)
-#if DB_SETUP_STAT == 0:
-load_schema("app/db/bitbox_schema.sql")
-scan_music_files()
-os.environ['DB_SETUP_STATUS'] = 1 # stop setup process from repeating
-#elif DB_SETUP_STAT == 1:
-    #print("[INFO] Database already set, skipping setup process.")
-#else:
-    #print("[ERROR] DB_SETUP_STAT env variable is not valid. Database not loaded.")
+if DB_SETUP_STAT == "0":
+    load_schema("app/db/bitbox_schema.sql")
+    scan_music_files()
+    os.environ['DB_SETUP_STATUS'] = "1" # stop setup process from repeating
+elif DB_SETUP_STAT == "1":
+    print("[INFO] Database already set, skipping setup process.")
+else:
+    print("[ERROR] DB_SETUP_STAT env variable is not valid. Database not loaded.")
 
 from routes import api, APIRoutes
 app = Flask(__name__)
