0598c61b3f24115ae72bb5241968b189a349ba1a
cra-y <scavonekeegan@gmail.com>
2025-06-21 13:07:12 -0400

    feat(backend): Adds backend functionality to search screen

--- bitbox/app/SearchScreen.tsx
+++ bitbox/app/SearchScreen.tsx
@@ -12,7 +12,7 @@ export default function SearchScreen() {
     useEffect(() => {
         const fetchPlaylists = async () => {
             try {
-                const response = await fetch("http://localhost:3000/playlists");
+                const response = await fetch("http://127.0.0.1:5000/playlists");
                 const data = await response.json();
                 setPlaylists(data);
             } catch (error) {
